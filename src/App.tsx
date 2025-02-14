061a618084eb4b82fe13b5bb8ead24b4709aa045
Júlia Canas Llunell <juliacanas@MacBook-Air-de-Julia.local>
2025-02-14 18:59:13 +0100

    chore: install styled-components and add normalize css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
 import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
 import router from './routes'
+import theme from './styles/theme'
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   )
 }
