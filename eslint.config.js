4a63b8060d996502ad1bae8b9ff62869018a9803
Júlia Canas Llunell <juliacanas@MacBook-Air-de-Julia.local>
2025-02-14 15:14:30 +0100

    chore: add configuration for Prettier, ESLint, and Husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,42 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import tsEslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettierConfig from 'eslint-config-prettier'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist', 'node_modules'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
+      parser: tsParser,
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': tsEslint,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tsEslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'jsx-a11y/no-static-element-interactions': 'warn',
     },
   },
-)
+  prettierConfig,
+]
